82071f854cac984f7e4cca4f4af3a63459a45558
Jamshidkhuja <jamshidxojab@gmail.com>
2020-08-01 14:41:30 +0500

    added methods

--- README.md
+++ README.md
@@ -11,5 +11,15 @@ Generates random number in a given range
 ```
 const r = require('genrannum');
 
-r(5);
+r(method, n);
+
+r('nomethod', 5) // Math.random() * 5
+
+r('trunc', 5) // Math.trunc(Math.random() * 5)
+
+r('floor', 5) // Math.floor(Math.random() * 5)
+
+r('ceil', 5) // Math.ceil(Math.random() * 5)
+
+r('round', 5) // Math.round(Math.random() * 5)
 ```
--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-function genRanNum(n) {
+function genRanNum(method, n) {
   if (typeof n !== "number") {
     return "ERROR💥: Only numbers";
   }
-  return Math.round(Math.random() * n);
+
+  if (method === "nomethod") {
+    return Math.random() * n;
+  } else if (method === "floor") {
+    return Math.floor(Math.random() * n);
+  } else if (method === "ceil") {
+    return Math.ceil(Math.random() * num);
+  } else if (method === "trunc") {
+    return Math.trunc(Math.random() * n);
+  } else if (method === "round") {
+    return Math.round(Math.random() * n);
+  }
 }
 
 module.exports = genRanNum;
